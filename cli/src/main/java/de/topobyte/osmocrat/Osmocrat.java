10194bd17ed9f1ec362427b3267ea678778fb7ba
Sebastian Kürten <sebastian@topobyte.de>
2017-08-31 01:24:34 +0200

    Outline main executable

---
+++ cli/src/main/java/de/topobyte/osmocrat/Osmocrat.java
@@ -0,0 +1,55 @@
+// Copyright 2017 Sebastian Kuerten
+//
+// This file is part of osmocrat.
+//
+// osmocrat is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osmocrat is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with osmocrat. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.osmocrat;
+
+import de.topobyte.utilities.apache.commons.cli.commands.ArgumentParser;
+import de.topobyte.utilities.apache.commons.cli.commands.ExeRunner;
+import de.topobyte.utilities.apache.commons.cli.commands.ExecutionData;
+import de.topobyte.utilities.apache.commons.cli.commands.RunnerException;
+import de.topobyte.utilities.apache.commons.cli.commands.options.DelegateExeOptions;
+import de.topobyte.utilities.apache.commons.cli.commands.options.ExeOptions;
+import de.topobyte.utilities.apache.commons.cli.commands.options.ExeOptionsFactory;
+
+public class Osmocrat
+{
+
+	public static ExeOptionsFactory OPTIONS_FACTORY = new ExeOptionsFactory() {
+
+		@Override
+		public ExeOptions createOptions()
+		{
+			DelegateExeOptions options = new DelegateExeOptions();
+			options.addCommand("gui", OsmocratGui.OPTIONS_FACTORY,
+					OsmocratGui.class);
+			return options;
+		}
+
+	};
+
+	public static void main(String[] args) throws RunnerException
+	{
+		ExeOptions options = OPTIONS_FACTORY.createOptions();
+		ArgumentParser parser = new ArgumentParser("osmocrat", options);
+
+		ExecutionData data = parser.parse(args);
+		if (data != null) {
+			ExeRunner.run(data);
+		}
+	}
+
+}
---
+++ cli/src/main/java/de/topobyte/osmocrat/OsmocratGui.java
@@ -0,0 +1,77 @@
+// Copyright 2017 Sebastian Kuerten
+//
+// This file is part of osmocrat.
+//
+// osmocrat is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// osmocrat is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with osmocrat. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.osmocrat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+
+import de.topobyte.osm4j.core.access.OsmIterator;
+import de.topobyte.osm4j.core.dataset.InMemoryListDataSet;
+import de.topobyte.osm4j.core.dataset.ListDataSetLoader;
+import de.topobyte.osm4j.utils.FileFormat;
+import de.topobyte.osm4j.utils.OsmIoUtils;
+import de.topobyte.utilities.apache.commons.cli.OptionHelper;
+import de.topobyte.utilities.apache.commons.cli.commands.args.CommonsCliArguments;
+import de.topobyte.utilities.apache.commons.cli.commands.options.CommonsCliExeOptions;
+import de.topobyte.utilities.apache.commons.cli.commands.options.ExeOptions;
+import de.topobyte.utilities.apache.commons.cli.commands.options.ExeOptionsFactory;
+
+public class OsmocratGui
+{
+
+	private static final String OPTION_INPUT = "input";
+
+	public static ExeOptionsFactory OPTIONS_FACTORY = new ExeOptionsFactory() {
+
+		@Override
+		public ExeOptions createOptions()
+		{
+			Options options = new Options();
+			// @formatter:off
+			OptionHelper.addL(options, OPTION_INPUT, true, true, "file", "an OSM data file");
+			// @formatter:on
+			return new CommonsCliExeOptions(options, "[options]");
+		}
+
+	};
+
+	public static void main(String name, CommonsCliArguments arguments)
+			throws IOException
+	{
+		CommandLine line = arguments.getLine();
+
+		String argInput = line.getOptionValue(OPTION_INPUT);
+		Path pathInput = Paths.get(argInput);
+
+		System.out.println("Loading data from file: " + pathInput);
+		OsmIterator input = OsmIoUtils.setupOsmIterator(
+				Files.newInputStream(pathInput), FileFormat.TBO, true);
+		InMemoryListDataSet data = ListDataSetLoader.read(input, true, true,
+				true);
+		System.out
+				.println(String.format("Loaded %d nodes, %d ways, %d relations",
+						data.getNodes().size(), data.getWays().size(),
+						data.getRelations().size()));
+	}
+
+}
